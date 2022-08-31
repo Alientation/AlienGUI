2aa2af17b8efbd2f52da3d63f810e2702bb0e5e0
Bill <billma.texas@gmail.com>
2022-08-30 20:20:44 -0500

    implemented resizingLines with variable constraints by line

--- src/com/alientation/aliengui/component/text/TextComponent.java
+++ src/com/alientation/aliengui/component/text/TextComponent.java
@@ -20,10 +20,14 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class TextComponent extends Component {
 
+    /**
+     * The update method of this text component
+     */
     enum TextUpdateState {
         DYNAMIC_WRAPPING_WORDS, //Whether to dynamically wrap text
         DYNAMIC_WRAPPING_CHARACTERS, //Whether dynamic wrapping will wrap characters around
-        DYNAMIC_RESIZING //Whether to dynamically resize text if dynamic wrapping is disabled
+        DYNAMIC_RESIZING, //Whether to dynamically resize text if dynamic wrapping is disabled
+        NONE
     }
 
     //Text to be displayed by lines
@@ -43,9 +47,11 @@ public class TextComponent extends Component {
     protected TextUpdateState textUpdateState = TextUpdateState.DYNAMIC_WRAPPING_WORDS;
 
     /**
-     * Constructs a multi lined Text Component
+     * Constructs a TextComponent object
      *
-     * @param text  Text lines
+     * @param textUpdateState       TextUpdateState of how to resize text
+     * @param fontRenderContext     FontRenderContext of how to render the text's font
+     * @param text                  Text to be displayed by this text component object
      */
     public TextComponent(TextUpdateState textUpdateState, FontRenderContext fontRenderContext, AttributedString... text) {
         this(text);
@@ -53,20 +59,44 @@ public class TextComponent extends Component {
         this.textUpdateState = textUpdateState;
     }
 
+    /**
+     * Constructs a TextComponent object with a base FontRenderContext with no effects on the text's render
+     *
+     * @param textUpdateState   TextUpdateState of how to resize text
+     * @param text              Text to be displayed by this text component object
+     */
     public TextComponent(TextUpdateState textUpdateState, AttributedString... text) {
         this(text);
         this.textUpdateState = textUpdateState;
     }
 
+    /**
+     * Constructs a TextComponent object with a base FontRenderContext with no effects on the text's render
+     * and a default dynamic word wrapping TextUpdateState
+     *
+     * @param text  Text to be displayed by this text component object
+     */
     public TextComponent(AttributedString... text) {
         linedText = new ArrayList<>(Arrays.stream(text).toList());
     }
 
+    /**
+     * Constructs a TextComponent object with a base FontRenderContext with no effects on the text's render
+     *
+     * @param textUpdateState   TextUpdateState of how to resize text
+     * @param text              Text to be displayed by this text component object
+     */
     public TextComponent(TextUpdateState textUpdateState, String... text) {
         this(text);
         this.textUpdateState = textUpdateState;
     }
 
+    /**
+     * Constructs a TextComponent object with a base FontRenderContext with no effects on the text's render
+     * and a default dynamic word wrapping TextUpdateState
+     *
+     * @param text  Text to be displayed by this text component object
+     */
     public TextComponent(String... text) {
         linedText = new ArrayList<>();
         for (String s : text)
@@ -87,7 +117,7 @@ public class TextComponent extends Component {
         FontRenderContext fontRenderContext = this.fontRenderContext;
         switch (textUpdateState) {
             case DYNAMIC_WRAPPING_WORDS, DYNAMIC_WRAPPING_CHARACTERS -> textToDisplay = wrappedLines(view.safeWidth());
-            case DYNAMIC_RESIZING -> fontRenderContext = resizedLines(view.safeHeight(),view.safeWidth());
+            case DYNAMIC_RESIZING -> fontRenderContext = resizedLines(view.safeWidth(), view.safeHeight());
             //TODO dynamic resizing for x, y, and both (more options)
         }
 
@@ -104,15 +134,31 @@ public class TextComponent extends Component {
     }
 
     //https://stackoverflow.com/questions/258486/calculate-the-display-width-of-a-string-in-java
+
+    /**
+     * Helper to wrap lines
+     *
+     * @param maxWidths MaxWidths of each line
+     * @return          The new linedText
+     */
     private List<AttributedString> wrappedLines(int... maxWidths) {
         List<AttributedString> wrappedString = new ArrayList<>();
 
+        //loop through and wrap each lines with a corresponding maxWidth
+        //if the next maxWidth does not exist (int the parameter maxWidths array), use the previous maxWidth
         for (int realLine = 0; realLine < linedText.size(); realLine++)
             wrappedString.addAll(wrappedLine(linedText.get(realLine),maxWidths[Math.min(realLine,maxWidths.length-1)]));
 
         return wrappedString;
     }
 
+    /**
+     * Helper to wrap lines
+     *
+     * @param attributedString  Original text
+     * @param maxWidth          Max width of this line
+     * @return                  The new linedText for this text
+     */
     private List<AttributedString> wrappedLine(AttributedString attributedString, int maxWidth) {
         //final result
         List<AttributedString> wrappedString = new ArrayList<>();
@@ -129,26 +175,58 @@ public class TextComponent extends Component {
         AttributedCharacterIterator characterCollector = attributedString.getIterator();
 
         while (lineBreakMeasurer.getPosition() < end) { //while current attributed char is still within the attributed string
+            //get the next slotted text within the maxWidth
             TextLayout textLayout = lineBreakMeasurer.nextLayout(maxWidth);
 
+            //keep track of the new wrapped string's wrapped lines
             wrappedString.add(new AttributedString(characterCollector, characterCollector.getIndex(), lineBreakMeasurer.getPosition()));
         }
 
         return wrappedString;
     }
 
-    private FontRenderContext resizedLines(int maxHeight, int maxWidth) {
+    /**
+     * Constructs a scaled FontRenderContext given width and height scale factors
+     *
+     * @param scaleFactorWidth  Scale factor of the width
+     * @param scaleFactorHeight Scale factor of the height
+     * @return                  Scaled FontRenderContext
+     */
+    public FontRenderContext getScaledFontRenderContext(float scaleFactorWidth, float scaleFactorHeight) {
+        //update render technique (affine transform)
+        AffineTransform scale = AffineTransform.getScaleInstance(scaleFactorWidth,scaleFactorHeight);
+
+        //check if the current renderContext has a transform, if so, scale this transform
+        if (fontRenderContext.getTransform() != null) scale.concatenate(fontRenderContext.getTransform());
+
+        //returns a new font render context
+        return new FontRenderContext(scale, fontRenderContext.getAntiAliasingHint(),
+                fontRenderContext.getFractionalMetricsHint());
+    }
+
+    /**
+     * Resizes lines given the constraints
+     *
+     * @param maxWidth  Width constraint
+     * @param maxHeight Height constraint
+     * @return          Scaled FontRenderContext
+     */
+    private FontRenderContext resizedLines(int maxWidth, int maxHeight) {
+        if (maxHeight <= 0 || maxWidth <= 0)
+            throw new IllegalStateException("TextComponent::resizedLines must have valid constraints");
 
         //find the current minimum width and height of the current text's render
-        int currentWidth = 0;
-        int currentHeight = 0;
+        float currentWidth = 0;
+        float currentHeight = 0;
 
+        //for each line
         for (AttributedString attributedString : linedText) {
             TextLayout textLayout = new TextLayout(attributedString.getIterator(),fontRenderContext);
 
-            currentHeight += textLayout.getAscent();
-            currentHeight += textLayout.getDescent() + textLayout.getLeading();
+            //add all height measurements
+            currentHeight += textLayout.getAscent() + textLayout.getDescent() + textLayout.getLeading();
 
+            //update the currentWidth to be the largest width
             currentWidth = Math.max(currentWidth,(int) textLayout.getAdvance());
         }
 
@@ -165,24 +243,79 @@ public class TextComponent extends Component {
         float scaleFactor = Math.min(maxWidth/currentWidth,maxHeight/currentHeight);
 
         //update render technique (affine transform)
-        AffineTransform scale = AffineTransform.getScaleInstance(scaleFactor,scaleFactor);
-
-        if (fontRenderContext.getTransform() != null) scale.concatenate(fontRenderContext.getTransform());
-
-        return new FontRenderContext(scale, fontRenderContext.getAntiAliasingHint(),
-                fontRenderContext.getFractionalMetricsHint());
+        return getScaledFontRenderContext(scaleFactor,scaleFactor);
     }
 
-    //TODO implement
-    private List<AttributedString> resizedLines(Collection<Integer> maxHeights, Collection<Integer> maxWidths) {
+    /**
+     * Resizes lines given the constraints by line
+     *
+     * @param maxLineWidthsCollection   Width constraint per line
+     * @param maxLineHeightsCollection  Height constraint per line
+     * @return                          Scaled FontRenderContext
+     */
+    private FontRenderContext resizedLines(Collection<Integer> maxLineWidthsCollection, Collection<Integer> maxLineHeightsCollection) {
+        //TODO determine whether to keep these state checkers for easier debugging for the user
+        //sanity checks to help user debug
+        if (maxLineHeightsCollection == null || maxLineHeightsCollection.size() == 0)
+            throw new IllegalStateException("TextComponent::resizedLines maxLineHeightsCollection collection must be initialized with values");
+        if (maxLineWidthsCollection == null || maxLineWidthsCollection.size() == 0)
+            throw new IllegalStateException("TextComponent::resizedLines maxLineWidthsCollection collection must be initialized with values");
 
-        return null;
+        //convert to arraylist
+        ArrayList<Integer> maxLineHeights = new ArrayList<>(maxLineHeightsCollection);
+        ArrayList<Integer> maxLineWidths = new ArrayList<>(maxLineWidthsCollection);
+
+        //find the min scale factor
+        float minScaleFactor = 1f;
+        int maxHeightIndex = 0, maxWidthIndex = 0; //indexes of the maxHeight and maxWidth in the arraylist collection
+        for (AttributedString attributedString : linedText) {
+            TextLayout textLayout = new TextLayout(attributedString.getIterator(),fontRenderContext);
+
+            //help visualize the math
+            int maxWidth = maxLineWidths.get(maxWidthIndex);
+            int maxHeight = maxLineHeights.get(maxHeightIndex);
+            float currentWidth = textLayout.getAdvance();
+            float currentHeight = textLayout.getAscent() + textLayout.getDescent() + textLayout.getLeading();
+
+            //current scale factor required to resize
+            float scaleFactor = Math.min(maxWidth/currentWidth,maxHeight/currentHeight);
+
+            //find the smallest scale factor needed to resize this linedText
+            minScaleFactor = Math.min(scaleFactor,minScaleFactor);
+
+            //increment maxHeight and maxWidth indexes if possible
+            if (maxHeightIndex < maxLineHeights.size() - 1) maxHeightIndex++;
+            if (maxWidthIndex < maxLineWidths.size() - 1) maxWidthIndex++;
+        }
+
+        //update render technique (affine transform)
+        return getScaledFontRenderContext(minScaleFactor,minScaleFactor);
     }
 
-    //TODO implement
-    private List<AttributedString> resizedLines(int[] maxHeights, int[] maxWidths) {
+    /**
+     * Resizes lines given the constraints
+     *
+     * @param maxLineWidths     Width constraint per line
+     * @param maxLineHeights    Height constraint per line
+     * @return                  Scaled FontRenderContext
+     */
+    private FontRenderContext resizedLines(int[] maxLineWidths, int[] maxLineHeights) {
+        //TODO determine whether to keep these state checkers for easier debugging for the user
+        //sanity checks to help user debug
+        if (maxLineHeights == null || maxLineHeights.length == 0)
+            throw new IllegalStateException("TextComponent::resizedLines maxLineHeights collection must be initialized with values");
+        if (maxLineWidths == null || maxLineWidths.length == 0)
+            throw new IllegalStateException("TextComponent::resizedLines maxLineWidths collection must be initialized with values");
 
-        return null;
+        //Arraylist collection to pass it to the other overloaded method
+        ArrayList<Integer> newMaxLineHeights = new ArrayList<>(maxLineHeights.length);
+        ArrayList<Integer> newMaxLineWidths = new ArrayList<>(maxLineWidths.length);
+
+        for (int maxLineHeight : maxLineHeights) newMaxLineHeights.add(maxLineHeight);
+        for (int maxLineWidth : maxLineWidths) newMaxLineWidths.add(maxLineWidth);
+
+        //reuse the overloaded method's code
+        return resizedLines(newMaxLineWidths,newMaxLineHeights);
     }
 
 
