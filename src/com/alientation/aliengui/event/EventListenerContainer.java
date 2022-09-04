ae2ece3e3d9036c395a208229990d54bd5f85436
Bill <billma.texas@gmail.com>
2022-09-03 22:12:25 -0500

    ensured that last priority spot will be ordered from most recent to the eldest registered listener

--- src/com/alientation/aliengui/event/EventListenerContainer.java
+++ src/com/alientation/aliengui/event/EventListenerContainer.java
@@ -12,9 +12,6 @@ import java.util.*;
 @SuppressWarnings("unused")
 public class EventListenerContainer<T extends EventListener> {
     public static final int PRIORITY_FIRST = 0;
-
-    //TODO internal system listeners who register priority last will not always be the last listener
-    //this is because of the way dispatching iterates through the contained event listeners
     public static final int PRIORITY_LAST = 1000;
     private final TreeMap<Integer, ArrayList<T>> eventListenerMap = new TreeMap<>(Comparator.reverseOrder());
 
@@ -24,34 +21,35 @@ public class EventListenerContainer<T extends EventListener> {
                 dispatch.dispatch(listener);
     }
 
-    public boolean addListener(T listener, int priority) {
+    public void addListener(T listener, int priority) {
         if (priority < PRIORITY_FIRST) priority = PRIORITY_FIRST;
         if (priority > PRIORITY_LAST) priority = PRIORITY_LAST;
         removeListener(listener);
         eventListenerMap.computeIfAbsent(priority, k -> new ArrayList<>());
-        return eventListenerMap.get(priority).add(listener);
+
+        //ensure first ones to add to the last spot will be at the end of the list
+        if (priority == PRIORITY_LAST) eventListenerMap.get(priority).add(0, listener);
+        else eventListenerMap.get(priority).add(listener);
     }
 
-    public boolean addListenerAtEnd(T listener) {
+    public void addListenerAtEnd(T listener) {
         int currentEnd = eventListenerMap.lastKey() + 1;
-        if (currentEnd > PRIORITY_LAST) currentEnd = PRIORITY_LAST;
-        return addListener(listener,currentEnd+1);
+        addListener(listener,currentEnd+1);
     }
 
-    public boolean addListenerAtBeginning(T listener) {
+    public void addListenerAtBeginning(T listener) {
         int currentBeginning = eventListenerMap.firstKey() - 1;
         if (currentBeginning < PRIORITY_FIRST) currentBeginning = PRIORITY_FIRST;
-        return addListener(listener,currentBeginning);
+        addListener(listener,currentBeginning);
     }
 
-    public boolean addListener(T listener) {
-        return addListenerAtEnd(listener);
+    public void addListener(T listener) {
+        addListenerAtEnd(listener);
     }
 
-    public boolean removeListener(T listener) {
+    public void removeListener(T listener) {
         for (int i : eventListenerMap.keySet())
-            return eventListenerMap.get(i).remove(listener);
-        return false;
+            eventListenerMap.get(i).remove(listener);
     }
 
     public boolean containsListener(T listener) {
