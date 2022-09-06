fa52066a17e689a05e41aef8c4e7d6626a71255c
Bill <billma.texas@gmail.com>
2022-09-05 19:57:58 -0500

    JUnit for testing

---
+++ src/test/java/com/aliengui/component/dimension/DimensionComponentTests.java
@@ -0,0 +1,12 @@
+package com.aliengui.component.dimension;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DimensionComponentTests {
+    @Test
+    @DisplayName("testing creating a test")
+    void test() {
+
+    }
+}
