64ed28db59617adfa3e88724bfeda69d1f459b73
Bill <billma.texas@gmail.com>
2022-09-06 20:57:06 -0500

    testing junit tests

--- src/test/java/com/aliengui/component/dimension/DimensionComponentTests.java
+++ src/test/java/com/aliengui/component/dimension/DimensionComponentTests.java
@@ -1,12 +1,36 @@
 package com.aliengui.component.dimension;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.*;
+import org.junit.runners.Suite;
+
+@Suite.SuiteClasses(DimensionComponentTests.class)
 public class DimensionComponentTests {
+
+    @BeforeAll
+    public static void setup() {
+        System.out.println("setup tests");
+    }
+
+    @BeforeEach
+    public void init() {
+        System.out.println("starting a test");
+    }
+
     @Test
     @DisplayName("testing creating a test")
-    void test() {
+    //@Disabled("Just a test")
+    public void test() {
+        System.out.println("running a test");
+    }
 
+    @AfterEach
+    public void tearDown() {
+        System.out.println("finished a test");
+    }
+
+    @AfterAll
+    public static void done() {
+        System.out.println("done with all tests");
     }
 }
